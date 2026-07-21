412b05896c08196348ca0e08b5fcfc0f673f154c
zoro-sudo-cloud <zoro081106@gmail.com>
2026-07-21 09:59:15 +0530

    exp2website

---
+++ script.js
@@ -0,0 +1,41 @@
+function registerUser(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
+
+if(name=="" || email=="" || password==""){
+    alert("Please fill all fields");
+}
+else{
+    alert("Registration Successful");
+    window.location="login.html";
+}
+}
+
+function loginUser(){
+
+let email=document.getElementById("loginEmail").value;
+let password=document.getElementById("loginPassword").value;
+
+if(email=="" || password=="")
+{
+    alert("Please enter Email and Password");
+}
+else
+{
+    alert("Login Successful");
+    window.location="index.html";
+}
+
+}
+function enroll(button, video) {
+    if (button.innerHTML === "Enroll") {
+        alert("Enrolled Successfully!");
+        button.innerHTML = "Start Learning...";
+        button.style.backgroundColor = "blue";
+    } else {
+        window.open(video, "_blank");
+    }
+
+}
